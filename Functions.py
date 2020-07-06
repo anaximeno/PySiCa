0d9ec5725de4d506763d118f874dbb33b9e25836
anaximenobrito <anaximenobrito@gmail.com>
2020-07-06 11:49:35 -0100

    changed name and some (trad also) codes

--- Functions.py
+++ Functions.py
@@ -1,37 +1,54 @@
-from math import trunc
+from math import trunc, sqrt
 
 
-def adicao(x, y):
+def addition(x, y):
     if (x + y) % 1 == 0:
         return trunc(x + y)
     else:
-        return x + y
+        if x < 1 and x > 0 or y < 1 and y > 0:
+            return (x * 10 + y * 10) / 10
+        else:    
+            return x + y
 
 
-def subtracao(x, y):
+def subtraction(x, y):
     if (x - y) % 1 == 0:
         return trunc(x - y)
     else:
-        return x - y
+        if x < 1 and x > 0 or y < 1 and y > 0:
+            return (x * 10 - y * 10) / 10
+        else:
+            return x - y
 
 
-def multiplicacao(x, y):
+def multiplication(x, y):
     if (x * y) % 1 == 0:
         return trunc(x * y)
     else:
-        return x * y
+        if x < 1 and x > 0 or y < 1 and y > 0:
+            return ((x * 10) * (y * 10)) / 10
+        else:
+            return x * y
 
 
-def divisao(x, y):
+def division(x, y):
     if (x / y) % 1 == 0:
         return trunc(x / y)
     else:
-        return x / y
+        if x < 1 and x > 0 or y < 1 and y > 0:
+            return ((x * 10) / (y * 10)) / 10
+        else:
+            return x / y
 
 
-def potencia(x, y):
+def power(x, y):
     if pow(x, y) % 1 == 0:
         return trunc(pow(x, y))
     else:
         return pow(x, y)
 
+def square(x):
+    if sqrt(x) % 1 == 0:
+        return trunc(sqrt(x))
+    else:
+        return sqrt(x)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Simple Calculator -> PySiCa
+
+This was a simple in code calculator made by me, in the python studying.
