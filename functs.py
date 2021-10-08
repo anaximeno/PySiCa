7d4a785a181b77a2ff2ca30255145a33735b1151
Anaxímeno Brito <anaximenobrito@gmail.com>
2021-10-08 16:44:39 -0100

    small fix on the vocabulary

--- functs.py
+++ functs.py
@@ -87,7 +87,7 @@ class BinaryOperation(Expr):
         return f'{self._OPERATION_NAME}({self.left.__repr__()}, {self.right.__repr__()})'
     
     def _is_var(self):
-        """Returns if this Binary operation is variable (if it as a variable)"""
+        """Returns if this Binary operation is variable (if it has a variable)"""
         return self.left._is_var() or self.right._is_var()
     
     def get_variables(self) -> set:
@@ -206,7 +206,7 @@ def eval_expr(expr: str, vars_allowed: bool = False) -> Expr:
         (Sub._OPERATION_SYMBOL, Sub),
         (Times._OPERATION_SYMBOL, Times),
         (Div._OPERATION_SYMBOL, Div)
-        ])
+    ])
 
     for symbol in BinaryOperation._ALLOWED_OPERATION_SYMBOLS:
         sep = expr.split(symbol)
