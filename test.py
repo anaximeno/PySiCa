0edf6cd627aa85aea65e75b304f35dc7db10a315
anaximeno <anaximenobrito@gmail.com>
2021-12-23 23:53:51 -0100

    made some adaptations

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 """Unit tests for PySiCa."""
-from common import *
+from expressions import (
+    BinaryOperation,
+    Expression,
+    Const,
+    Mult,
+    Var,
+    Div,
+    Add,
+    Sub,
+    expr,
+)
 
 
 def test_add():
@@ -22,39 +32,39 @@ def test_div():
 
 
 def test_full_expr():
-    expr = Add(Mult(8, Div(50, 25)), Sub(12, 8))
-    assert expr.eval() == 20
+    expression = Add(Mult(8, Div(50, 25)), Sub(12, 8))
+    assert expression.eval() == 20
 
 
-def test_full_expr_with_vars():
-    expr = Add(Mult('x', 'x'), Div(5, Sub('x', 'y')))
+def test_full_expression_with_vars():
+    expression = Add(Mult('x', 'x'), Div(5, Sub('x', 'y')))
     env = {
         'x': 2,
         'y': 1
     }
-    assert expr.eval(env) == 9
+    assert expression.eval(env) == 9
 
 
-def test_parse_expr():
-    expr = parse_expression('2 - 5 / 25 - 7 + 3')
-    assert expr.eval() == -2.2
+def test_parse_expression():
+    expression = expr('2 - 5 / 25 - 7 + 3')
+    assert expression.eval() == -2.2
 
 
 def test_parse_with_vars():
-    expr = parse_expression('5 - x - 9 + y', allow_vars=True)
+    expression = expr('5 - x - 9 + y', allow_variables=True)
     env = {
         'x': 3,
         'y': 12
     }
-    assert expr.eval(env) == 5
+    assert expression.eval(env) == 5
 
 
 def test_parse_beginning_with_minus():
-    expr = parse_expression('- 8 + 3')
-    assert expr.eval() == -5
+    expression = expr('- 8 + 3')
+    assert expression.eval() == -5
 
 
 def test_float_values():
-    a = parse_expression ('0.569')
+    a = expr ('0.569')
     assert a == 0.569
 
